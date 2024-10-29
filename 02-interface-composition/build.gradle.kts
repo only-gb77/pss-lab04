9f0037f5a408a211e7b79b632305aa533418c4a9
Gabriel <gabry.suzzi@gmail.com>
2024-10-29 11:52:43 +0100

    confirm changed kts Es 02

--- 02-interface-composition/build.gradle.kts
+++ 02-interface-composition/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
-    java
+  java // Carica il necessario per la compilazione di Java
+  application // Carica il necessario per l'esecuzione di classi con la JVM
+}
+application { // Configurazione dell'esecuzione, la main class è sufficiente
+    mainClass.set("it.unibo.Test")
 }
