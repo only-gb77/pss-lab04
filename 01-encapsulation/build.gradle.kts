cb9b8774f9107d5205ad5421a178f705a6d9c932
Gabriel <gabry.suzzi@gmail.com>
2024-10-29 11:51:25 +0100

    Changed build.gradle.kts for 01 and 02

--- 01-encapsulation/build.gradle.kts
+++ 01-encapsulation/build.gradle.kts
@@ -1,1 +1,7 @@
-plugins { java }
+plugins {
+  java // Carica il necessario per la compilazione di Java
+  application // Carica il necessario per l'esecuzione di classi con la JVM
+}
+application { // Configurazione dell'esecuzione, la main class è sufficiente
+    mainClass.set("it.unibo.encapsulation.TestAllClasses")
+}
